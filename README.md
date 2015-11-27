d8989e35fe7f444828ca829eeba5e450d5d39f63
Nahum Fabian <nahum_fabian13@hotmail.com>
2015-11-27 15:38:42 -0600

    readme was modified

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # inventory-api
 node js API to retrieve data from the Inventary system (POS)
+
+###Enpoints
+- GET /products
+- GET /clients
+- GET /products/most_sold
+- GET /clients/:name
+- POST /invoice/new
+- POST /authenticate
+- GET /logout
--- routes.js
+++ routes.js
@@ -20,7 +20,7 @@ router.get('/products/most_sold', function(req, res) {
 
 });
 
-router.get('/clients/', function(req, res) {
+router.get('/clients', function(req, res) {
   req.getConnection(function(err, conn) {
     if (err) return next("Cannot Connect");
 
@@ -35,19 +35,19 @@ router.get('/clients/', function(req, res) {
 });
 
 router.get('/clients/:name', function(req, res) {
-  
+  //TODO
 });
 
 router.post('/invoice/new', function(req, res) {
-
+  //TODO
 });
 
 router.post('/authenticate', function(req, res) {
-
+  //TODO
 });
 
 router.get('/logout', function(req, res) {
-
+  //TODO
 });
 
 module.exports = router;
