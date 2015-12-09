bb447d78db7bb49870f9ec9e050eb63aa6fcb3df
Nahum Fabian <nahum_fabian13@hotmail.com>
2015-12-09 03:38:13 -0600

    all products querie modified

--- SQLStatements.js
+++ SQLStatements.js
@@ -1,15 +1,31 @@
 module.exports = {
-  allProductsStmt: "select  p.producto_id, p.producto_nombre, m.nombre_marca, f.nombre_familia as categoria, \
-    		pro.proveedor_nombre, p.producto_estatus, p.producto_costo_unitario, p.producto_impuesto, \
-            i.cantidad as cantidad_inventario, i.id_inventario\
-    	from producto as p\
-    left join inventario i\
-    	on i.id_producto = p.producto_id\
-    left join marcas m\
-    	on m.id_marca = p.producto_marca\
-    left join familia f\
-    	on f.id_familia = p.producto_familia\
-    left join proveedor pro\
-    	on pro.id_proveedor = p.producto_proveedor\
-    order by p.producto_nombre asc"
+  allProductsStmt: "select p.producto_nombre as nombre, p.*, uhp.id_unidad, u.nombre_unidad, inventario.id_inventario,\
+  		inventario.id_local, inventario.cantidad, inventario.fraccion, l.nombre_linea, m.nombre_marca, \
+          f.nombre_familia, g.nombre_grupo, p2.proveedor_nombre, i.nombre_impuesto\
+  	from producto p\
+  left join lineas l\
+  	on l.id_linea = p.producto_linea\
+  left join marcas m\
+  	on m.id_marca = p.producto_marca\
+  left join familia f\
+  	on f.id_familia = p.producto_familia\
+  left join grupos g\
+  	on g.id_grupo = p.produto_grupo\
+  left join proveedor p2\
+  	on p2.id_proveedor = p.producto_proveedor\
+  left join impuestos i\
+  	on i.id_impuesto = p.producto_impuesto\
+  left join (SELECT DISTINCT inventario.id_producto, inventario.id_inventario, \
+  							inventario.cantidad, inventario.fraccion, \
+  							inventario.id_local \
+  				FROM inventario \
+  			WHERE inventario.id_local=1\
+  			 ORDER by id_inventario desc) as inventario\
+  	on inventario.id_producto = p.producto_id\
+  left join unidades_has_producto uhp\
+  	on uhp.producto_id = p.producto_id and uhp.orden = 1\
+  left join unidades u\
+  	on u.id_unidad = uhp.id_unidad\
+  where p.producto_estado in(0,1) and p.producto_estatus = 1\
+  group by p.producto_id"
 };
